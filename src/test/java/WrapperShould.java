739247eea9f755ebcd4c6b8271efae63d5b0eabd
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-11 12:34:33 +0100

    🔴newLine is before the end of the word

--- src/test/java/WrapperShould.java
+++ src/test/java/WrapperShould.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 "", 0->""
 "a",1->""
 "a",0->"\na"
+"a b",1->a\nb
  */
 
 public class WrapperShould {
@@ -20,6 +21,10 @@ public class WrapperShould {
     @Test
     void foo1(){
         assertEquals("\na", wrapper("a",0));
+    }
+    @Test
+    void foo2(){
+        assertEquals("a\nb", wrapper("a b",1));
     }
 
     private String wrapper(String text, int colum) {
