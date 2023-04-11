733e4f70b1ba56940ec8a8d3ef19abd22f3906f4
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-11 12:08:20 +0100

    🔴newLine is at the end of the word

--- src/test/java/WrapperShould.java
+++ src/test/java/WrapperShould.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 /*
 "", 0->""
-"",1->"";
+"a",1->"";
  */
 
 public class WrapperShould {
@@ -11,12 +11,16 @@ public class WrapperShould {
     @Test
     void allow_empty_text_and_colum_number_0(){
         assertEquals("", wrapper("",0));
-
+    }
+    @Test
+    void foo(){
+        assertEquals("a\n", wrapper("a",1));
     }
 
     private String wrapper(String text, int colum) {
         return text;
     }
+
 
 
 }
