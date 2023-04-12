7cefe5c0654ae783f7328a767098b1d8a4db0ef1
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-12 21:00:25 +0100

    🔴newLine coincides with an empty place

--- src/test/java/WrapperShould.java
+++ src/test/java/WrapperShould.java
@@ -3,59 +3,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 /*
 "", 0->""
-"a",1->""
-"a",0->"\na"
-"a b",1->a\nb
-"a b",2->a\nb
-"ab",1->\nab
+"abc dfg", 3->"abc\ndfg"
+"abc dfg", 2->"abc\ndfg"
+
+
 
  */
 
 public class WrapperShould {
 
     @Test
-    void allow_empty_text_and_colum_number_0(){
-        assertEquals("\n", wrapper("",0));
+    void allow_empty_text_and_colum_number_0() {
+        assertEquals("", wrapper("", 0));
     }
+
     @Test
-    void foo(){
-        assertEquals("a\n", wrapper("a",1));
+    void when_newLine_is_in_a_empty_place() {
+        assertEquals("abc\ndfg", wrapper("abc dfg", 3));
     }
-    @Test
-    void foo1(){
-        assertEquals("\na", wrapper("a",0));
-    }
-    @Test
-    void foo2(){
-        assertEquals("a\nb", wrapper("a b",1));
-    }
-    @Test
-    void foo3(){
-        assertEquals("a\nb", wrapper("a b",2));
-    }
+
+
+
+
+
+
 
 
 
     private String wrapper(String text, int colum) {
-        if (text.length()==colum){
-            return text= text+"\n";
+
+        if (colum==0){
+            return text;
         }
-
-        if (text.length()>colum) {
-            if (text.contains(" ")) {
-                String divisionPoint= String.valueOf(text.charAt(colum));
-                if (divisionPoint.equals(" ")){
-                    text = text.replace(text.charAt(colum), '\n');
-                }else{
-                    int cut= text.lastIndexOf(" ");
-                    text = text.replace(text.charAt(cut), '\n');
-                }
-
-            }else{
-                text="\n"+text;
-            }
-        }
-
 
         return text;
     }
