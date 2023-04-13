d952f3b3b0dea2b02842fefec41f2c95fc527439
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-13 12:39:14 +0100

    🟢 not cut entry when entryLength is equals colum size

--- src/test/java/WrapperShould.java
+++ src/test/java/WrapperShould.java
@@ -20,18 +20,9 @@ public class WrapperShould {
 
     @Test
     void foo() {
-        assertEquals("aaaa", wrapper("aaaa", 3));
+        assertEquals("aaaa", wrapper("aaaa", 4));
     }
 
-    @Test
-    void when_newLine_is_in_a_empty_place() {
-        assertEquals("abc\ndfg\nhijk", wrapper("abc dfg hijk", 3));
-    }
-
-    @Test
-    void muy_largo() {
-        assertEquals("Mas vale\ntarde que\nnunca hipopotamo", wrapper("Mas vale tarde que nunca hipopotamo, que te crees una margarita", 7));
-    }
 
 
     private String wrapper(String entry, int colum) {
@@ -39,29 +30,16 @@ public class WrapperShould {
         if (colum==0)
             return entry;
 
-        if (entry.length() == colum + 1)
+        if (entry.length() == colum )
             return entry;
 
-        int newCut = colum;
-        int restOfTextLength = entry.length();
-        while (entry.length() - newCut > colum) {
-            String firstText= entry.substring( 0, newCut);
-            String secondText = entry.substring (newCut).trim();
-            String cutting;
-            cutting = String.valueOf (entry.charAt (newCut));
-            String space = " ";
-            String lineBreak = "\n";
-            if (cutting.equals(space)) {
-                entry = firstText + lineBreak + secondText;
-            }else{
-                secondText = secondText.replaceFirst(space, lineBreak);
-                entry = firstText + secondText;
-                int lastLineBreak = entry.lastIndexOf(lineBreak);
-                secondText = entry.substring(lastLineBreak + 1);
-            }
-            restOfTextLength = secondText.length();
-            newCut = entry.lastIndexOf(lineBreak) + colum;
-        }
+        final String space = " ";
+        final String lineBreak = "\n";
+
+
+
+
+
         return entry;
     }
 }
