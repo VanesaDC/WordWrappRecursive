a1916e5cf64f6bbe2357e9f747721cae9d276584
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-05-31 17:53:30 +0100

    🔵wrapp() has been moved to wrapper.class

--- src/main/java/Wrapper.java
+++ src/main/java/Wrapper.java
@@ -1,5 +1,32 @@
 public class Wrapper {
-    public static void main(String[] args) {
 
+    public Wrapper() {
+    }
+
+    public String wrap(String text, int column) {
+        if(column < 0){
+            throw new IllegalArgumentException("Column must be greater than zero");
+        }
+
+        final String space = " ";
+        final String lineBreak = "\n";
+        int cutIndex = column;
+
+        boolean textLengthIsGreaterThanColumn = text.length() > column;
+        if (textLengthIsGreaterThanColumn){
+            String cutPoint = String.valueOf(text.charAt(cutIndex));
+            int beginIndex = 0;
+            boolean cutPointIsALetter = !cutPoint.equals(space);
+            if (cutPointIsALetter) {
+                String textToWrap = text.substring(beginIndex, cutIndex );
+                if ( textToWrap.contains( space )) {
+                    cutIndex = textToWrap.lastIndexOf( space );
+                }
+            }
+            String textProcessed = text.substring(beginIndex, cutIndex ) + lineBreak;
+            String restOfText = text.substring (cutIndex).trim();
+            return textProcessed + wrap(restOfText, column);
+        }
+        return text;
     }
 }
--- src/test/java/WrapperShould.java
+++ src/test/java/WrapperShould.java
@@ -12,61 +12,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 
 public class WrapperShould {
+    Wrapper wrapper = new Wrapper();
 
     @Test
     void allow_empty_text() {
-        assertEquals("", wrap("", 8));
+        assertEquals("", wrapper.wrap("", 8));
     }
 
     @Test
     void not_wrap_text_that_fits_with_columns() {
-        assertEquals("aaaa", wrap("aaaa", 4));
+        assertEquals("aaaa", wrapper.wrap("aaaa", 4));
     }
     @Test
     void wrap_text_without_spaces_that_does_not_fit_in_columns() {
-        assertEquals("aa\naa", wrap("aaaa", 2));
+        assertEquals("aa\naa", wrapper.wrap("aaaa", 2));
     }
     @Test
     void wrap_when_cutpoint_is_a_space() {
-        assertEquals("aaa\nbbbb", wrap("aaa bbbb", 4));
+        assertEquals("aaa\nbbbb", wrapper.wrap("aaa bbbb", 4));
     }
 
     @Test
     void wrap_text_in_the_previous_spaces_when_cutpoint_is_a_character() {
-        assertEquals("Mas\ndesde", wrap("Mas desde", 6));
+        assertEquals("Mas\ndesde", wrapper.wrap("Mas desde", 6));
     }
     @Test
     void wrap_text_when_not_are_previous_space() {
-        assertEquals("Muchac\nhitos", wrap("Muchachitos", 6));
+        assertEquals("Muchac\nhitos", wrapper.wrap("Muchachitos", 6));
     }
     @Test
     void wrap_text_when_not_are_previous_space2() {
         assertEquals("Muchachito\ns de los\npueblos\naltos, no\nse si por\nel frío o\npor\nsupercalif\nrajilistic\nospialidos\no",
-                wrap("Muchachitos de los pueblos altos, no se si por el frío o por supercalifrajilisticospialidoso", 10));
+                wrapper.wrap("Muchachitos de los pueblos altos, no se si por el frío o por supercalifrajilisticospialidoso", 10));
     }
 
 
-    private String wrap(String text, int column) {
 
-        final String space = " ";
-        final String lineBreak = "\n";
-        int cutIndex = column;
-
-        boolean textLengthIsGreaterThanColumn = text.length() > column;
-        if (textLengthIsGreaterThanColumn){
-            String cutPoint = String.valueOf(text.charAt(cutIndex));
-            int beginIndex = 0;
-            boolean cutPointIsALetter = !cutPoint.equals(space);
-            if (cutPointIsALetter) {
-                String textToWrap = text.substring(beginIndex, cutIndex );
-                if ( textToWrap.contains( space )) {
-                    cutIndex = textToWrap.lastIndexOf( space );
-                }
-            }
-            String textProcessed = text.substring(beginIndex, cutIndex ) + lineBreak;
-            String restOfText = text.substring (cutIndex).trim();
-            return textProcessed + wrap(restOfText, column);
-        }
-        return text;
-    }
 }
